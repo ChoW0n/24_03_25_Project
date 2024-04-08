0d6785725cde93bc4a8e552bfaa4861842878a8d
ChoW0n <seru2272@gmail.com>
2024-04-08 12:31:59 +0900

    [Feat] 24_04_08_Dodge_Bullet

--- Dodge/Assets/Skripts/PlayerController.cs
+++ Dodge/Assets/Skripts/PlayerController.cs
@@ -15,22 +15,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f,0f,speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        //수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //Vector3 속도를 (xSpeed, 0, zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        //리지드바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
     }
     public void Die()
     {
